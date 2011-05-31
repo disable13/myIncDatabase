ec34b8d8a710ac5ee292df2614f024737a58f9f8
Timoschenko Sergey <disable13@ovi.com>
2011-06-01 00:01:14 +0400

    added Russian translation

---
+++ en.ts
@@ -0,0 +1,308 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>DDbConfig</name>
+    <message>
+        <location filename="src/ui/ddbconfig.cpp" line="28"/>
+        <source>NULL</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/ddbconfig.cpp" line="44"/>
+        <source>Driver</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/ddbconfig.cpp" line="45"/>
+        <source>Hostname</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/ddbconfig.cpp" line="46"/>
+        <source>Port</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/ddbconfig.cpp" line="47"/>
+        <source>User</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/ddbconfig.cpp" line="48"/>
+        <source>Password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/ddbconfig.cpp" line="49"/>
+        <source>Database name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/ddbconfig.cpp" line="50"/>
+        <source>Options</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/ddbconfig.cpp" line="56"/>
+        <source>Ok</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/ddbconfig.cpp" line="61"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/ddbconfig.cpp" line="66"/>
+        <source>Test</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/ddbconfig.cpp" line="146"/>
+        <source>Not connected</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/ddbconfig.cpp" line="147"/>
+        <source>Can&apos;t create connection to SQL server.
+Error text: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DFooter</name>
+    <message>
+        <location filename="src/ui/dfooter.cpp" line="95"/>
+        <source>Complete!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dfooter.cpp" line="97"/>
+        <source>Uncomplete...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DSqlQuertyViewer</name>
+    <message>
+        <location filename="src/ui/dsqlquertyviewer.cpp" line="20"/>
+        <source>Type</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dsqlquertyviewer.cpp" line="24"/>
+        <source>All</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dsqlquertyviewer.cpp" line="29"/>
+        <source>Other</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dsqlquertyviewer.cpp" line="74"/>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dsqlquertyviewer.cpp" line="80"/>
+        <source>Source</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dsqlquertyviewer.cpp" line="87"/>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dsqlquertyviewer.cpp" line="91"/>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DWorkWidget</name>
+    <message>
+        <location filename="src/ui/dworkwidget.cpp" line="36"/>
+        <source>Project not be opened</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dworkwidget.cpp" line="43"/>
+        <source>Can&apos;t read UI file </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dworkwidget.cpp" line="49"/>
+        <source>Bad UI file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dworkwidget.cpp" line="68"/>
+        <source>Can&apos;t create button %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dworkwidget.cpp" line="73"/>
+        <location filename="src/ui/dworkwidget.cpp" line="82"/>
+        <location filename="src/ui/dworkwidget.cpp" line="85"/>
+        <source>Can&apos;t assign a value in the %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dworkwidget.cpp" line="197"/>
+        <source>Work widget error</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="35"/>
+        <source>File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="36"/>
+        <source>Create Project...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="39"/>
+        <source>Open Project...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="44"/>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="45"/>
+        <source>Exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="47"/>
+        <source>Project</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="48"/>
+        <location filename="src/ui/mainwindow.cpp" line="206"/>
+        <source>Connect...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="51"/>
+        <source>Connection settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="53"/>
+        <source>Query settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="55"/>
+        <source>User interface</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="64"/>
+        <source>Open MyInc Project File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="130"/>
+        <location filename="src/ui/mainwindow.cpp" line="140"/>
+        <source>Select MyInc Project</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="144"/>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="144"/>
+        <source>Can&apos;t read project file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="181"/>
+        <source>Connection to database...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="193"/>
+        <location filename="src/ui/mainwindow.cpp" line="226"/>
+        <source>Can&apos;t connect to SQL Server</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="206"/>
+        <source>Disconnect...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="229"/>
+        <source>Syntax error in URI query.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="232"/>
+        <source>SQL not be initialized.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="234"/>
+        <source>Config not be initialized.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="236"/>
+        <source>Namespace type error?!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="238"/>
+        <source>Config node not found in project xml file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="240"/>
+        <source>Root node not found in project xml file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="242"/>
+        <source>Syntax error in project xml file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="244"/>
+        <source>Can&apos;t open file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="248"/>
+        <source>Unknown</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MyIncApplication</name>
+    <message>
+        <location filename="src/core/myincapplication.cpp" line="36"/>
+        <source>--drivers	show aviable SQL drivers</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/core/myincapplication.cpp" line="37"/>
+        <source>--help	show this message</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ ru.ts
@@ -0,0 +1,312 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>DDbConfig</name>
+    <message>
+        <location filename="src/ui/ddbconfig.cpp" line="28"/>
+        <source>NULL</source>
+        <translatorcomment>Значиние пустое или не указано</translatorcomment>
+        <translation type="unfinished">Не Установлено</translation>
+    </message>
+    <message>
+        <location filename="src/ui/ddbconfig.cpp" line="44"/>
+        <source>Driver</source>
+        <translatorcomment>Драйвер для соединения с базой данных</translatorcomment>
+        <translation type="unfinished">Драйвер</translation>
+    </message>
+    <message>
+        <location filename="src/ui/ddbconfig.cpp" line="45"/>
+        <source>Hostname</source>
+        <translatorcomment>Имя хоста</translatorcomment>
+        <translation type="unfinished">Хост</translation>
+    </message>
+    <message>
+        <location filename="src/ui/ddbconfig.cpp" line="46"/>
+        <source>Port</source>
+        <translatorcomment>Номер порта</translatorcomment>
+        <translation type="unfinished">Порт</translation>
+    </message>
+    <message>
+        <location filename="src/ui/ddbconfig.cpp" line="47"/>
+        <source>User</source>
+        <translation type="unfinished">Пользователь</translation>
+    </message>
+    <message>
+        <location filename="src/ui/ddbconfig.cpp" line="48"/>
+        <source>Password</source>
+        <translation type="unfinished">Пароль</translation>
+    </message>
+    <message>
+        <location filename="src/ui/ddbconfig.cpp" line="49"/>
+        <source>Database name</source>
+        <translation type="unfinished">Имя базы данных</translation>
+    </message>
+    <message>
+        <location filename="src/ui/ddbconfig.cpp" line="50"/>
+        <source>Options</source>
+        <translation type="unfinished">Опции</translation>
+    </message>
+    <message>
+        <location filename="src/ui/ddbconfig.cpp" line="56"/>
+        <source>Ok</source>
+        <translation type="unfinished">ОК</translation>
+    </message>
+    <message>
+        <location filename="src/ui/ddbconfig.cpp" line="61"/>
+        <source>Cancel</source>
+        <translation type="unfinished">Отмена</translation>
+    </message>
+    <message>
+        <location filename="src/ui/ddbconfig.cpp" line="66"/>
+        <source>Test</source>
+        <translation type="unfinished">Проверка</translation>
+    </message>
+    <message>
+        <location filename="src/ui/ddbconfig.cpp" line="146"/>
+        <source>Not connected</source>
+        <translation type="unfinished">Нет соединения</translation>
+    </message>
+    <message>
+        <location filename="src/ui/ddbconfig.cpp" line="147"/>
+        <source>Can&apos;t create connection to SQL server.
+Error text: %1</source>
+        <translation type="unfinished">Невозможно установить соединение с SQL сервером.\nТекст ошибки: %1</translation>
+    </message>
+</context>
+<context>
+    <name>DFooter</name>
+    <message>
+        <location filename="src/ui/dfooter.cpp" line="95"/>
+        <source>Complete!</source>
+        <translation type="unfinished">Выполнено!</translation>
+    </message>
+    <message>
+        <location filename="src/ui/dfooter.cpp" line="97"/>
+        <source>Uncomplete...</source>
+        <translation type="unfinished">Ошибка выполнения...</translation>
+    </message>
+</context>
+<context>
+    <name>DSqlQuertyViewer</name>
+    <message>
+        <location filename="src/ui/dsqlquertyviewer.cpp" line="20"/>
+        <source>Type</source>
+        <translation type="unfinished">Тип</translation>
+    </message>
+    <message>
+        <location filename="src/ui/dsqlquertyviewer.cpp" line="24"/>
+        <source>All</source>
+        <translation type="unfinished">Все</translation>
+    </message>
+    <message>
+        <location filename="src/ui/dsqlquertyviewer.cpp" line="29"/>
+        <source>Other</source>
+        <translation type="unfinished">Остальное</translation>
+    </message>
+    <message>
+        <location filename="src/ui/dsqlquertyviewer.cpp" line="74"/>
+        <source>Name</source>
+        <translation type="unfinished">Имя</translation>
+    </message>
+    <message>
+        <location filename="src/ui/dsqlquertyviewer.cpp" line="80"/>
+        <source>Source</source>
+        <translation type="unfinished">Содержание</translation>
+    </message>
+    <message>
+        <location filename="src/ui/dsqlquertyviewer.cpp" line="87"/>
+        <source>Save</source>
+        <translation type="unfinished">Сохранить</translation>
+    </message>
+    <message>
+        <location filename="src/ui/dsqlquertyviewer.cpp" line="91"/>
+        <source>Close</source>
+        <translation type="unfinished">Закрыть</translation>
+    </message>
+</context>
+<context>
+    <name>DWorkWidget</name>
+    <message>
+        <location filename="src/ui/dworkwidget.cpp" line="36"/>
+        <source>Project not be opened</source>
+        <translation type="unfinished">Проект не открыт</translation>
+    </message>
+    <message>
+        <location filename="src/ui/dworkwidget.cpp" line="43"/>
+        <source>Can&apos;t read UI file </source>
+        <translation type="unfinished">Невозможно прочесть UI файл</translation>
+    </message>
+    <message>
+        <location filename="src/ui/dworkwidget.cpp" line="49"/>
+        <source>Bad UI file</source>
+        <translation type="unfinished">Не корректная структура UI файла</translation>
+    </message>
+    <message>
+        <location filename="src/ui/dworkwidget.cpp" line="68"/>
+        <source>Can&apos;t create button %1</source>
+        <translation type="unfinished">Не возможно создать кнопку %1</translation>
+    </message>
+    <message>
+        <location filename="src/ui/dworkwidget.cpp" line="73"/>
+        <location filename="src/ui/dworkwidget.cpp" line="82"/>
+        <location filename="src/ui/dworkwidget.cpp" line="85"/>
+        <source>Can&apos;t assign a value in the %1</source>
+        <translation type="unfinished">Не возможно присвоить значение в элементе %1</translation>
+    </message>
+    <message>
+        <location filename="src/ui/dworkwidget.cpp" line="197"/>
+        <source>Work widget error</source>
+        <translation type="unfinished">Ошибка рабочего виджета</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="35"/>
+        <source>File</source>
+        <translation type="unfinished">Файл</translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="36"/>
+        <source>Create Project...</source>
+        <translation type="unfinished">Создать проект...</translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="39"/>
+        <source>Open Project...</source>
+        <translation type="unfinished">Открыть проект...</translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="44"/>
+        <source>Close</source>
+        <translation type="unfinished">Закрыть</translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="45"/>
+        <source>Exit</source>
+        <translation type="unfinished">Выход</translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="47"/>
+        <source>Project</source>
+        <translation type="unfinished">Проект</translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="48"/>
+        <location filename="src/ui/mainwindow.cpp" line="206"/>
+        <source>Connect...</source>
+        <translation type="unfinished">Соединение...</translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="51"/>
+        <source>Connection settings</source>
+        <translation type="unfinished">Настройки соединения</translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="53"/>
+        <source>Query settings</source>
+        <translation type="unfinished">Настройки SQL запросов</translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="55"/>
+        <source>User interface</source>
+        <translation type="unfinished">Пользовательский интерфейс</translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="64"/>
+        <source>Open MyInc Project File</source>
+        <translation type="unfinished">Открыть файл проекта MyInc</translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="130"/>
+        <location filename="src/ui/mainwindow.cpp" line="140"/>
+        <source>Select MyInc Project</source>
+        <translation type="unfinished">Выбрать файл проекта MyInc</translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="144"/>
+        <source>Error</source>
+        <translation type="unfinished">Ошибка</translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="144"/>
+        <source>Can&apos;t read project file.</source>
+        <translation type="unfinished">Невозможно прочесть файл проекта.</translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="181"/>
+        <source>Connection to database...</source>
+        <translation type="unfinished">Соединение с базой данных...</translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="193"/>
+        <location filename="src/ui/mainwindow.cpp" line="226"/>
+        <source>Can&apos;t connect to SQL Server</source>
+        <translation type="unfinished">Невозможно соединиться с SQL сервером</translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="206"/>
+        <source>Disconnect...</source>
+        <translation type="unfinished">Разъединение...</translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="229"/>
+        <source>Syntax error in URI query.</source>
+        <translation type="unfinished">Ошибка синтаксиса в URI запросе.</translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="232"/>
+        <source>SQL not be initialized.</source>
+        <translation type="unfinished">SQL не инициализирован.</translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="234"/>
+        <source>Config not be initialized.</source>
+        <translation type="unfinished">Конфигурация не инициализирована.</translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="236"/>
+        <source>Namespace type error?!</source>
+        <translation type="unfinished">Ошибка типа пространства имен?!</translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="238"/>
+        <source>Config node not found in project xml file.</source>
+        <translation type="unfinished">Конфигурационный блок не найден в xml файле проекта.</translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="240"/>
+        <source>Root node not found in project xml file.</source>
+        <translation type="unfinished">Корневой блок не найден в xml файле проекта.</translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="242"/>
+        <source>Syntax error in project xml file.</source>
+        <translation type="unfinished">Ошибка синтаксиса в xml файле проекта.</translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="244"/>
+        <source>Can&apos;t open file.</source>
+        <translation type="unfinished">Невозможно открыть файл.</translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.cpp" line="248"/>
+        <source>Unknown</source>
+        <translation type="unfinished">Неизвестно</translation>
+    </message>
+</context>
+<context>
+    <name>MyIncApplication</name>
+    <message>
+        <location filename="src/core/myincapplication.cpp" line="36"/>
+        <source>--drivers	show aviable SQL drivers</source>
+        <translation type="unfinished">--drivers\tпоказать доступные SQL драйвера</translation>
+    </message>
+    <message>
+        <location filename="src/core/myincapplication.cpp" line="37"/>
+        <source>--help	show this message</source>
+        <translation type="unfinished">--help\tпоказать это сообщение</translation>
+    </message>
+</context>
+</TS>
